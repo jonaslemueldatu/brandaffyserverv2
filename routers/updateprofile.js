61046100c9e83f92478ade81be4b0b1a8263b978
jonaslemueldatu <jonas.lemuel.datu@eee.upd.edu.ph>
2023-07-15 15:05:50 +0800

    fixed saving of schema for contact type

--- routers/updateprofile.js
+++ routers/updateprofile.js
@@ -12,7 +12,7 @@ const upload = multer({ dest: "uploads/" });
 const { uploadFile } = require("../modules/s3");
 
 router.post("/", upload.single("profile_picture"), async (req, res) => {
-console.log(req.body)
+  console.log(req.body);
   let result = "";
   if (req.file) {
     const file = req.file;
@@ -22,12 +22,10 @@ console.log(req.body)
   }
 
   affiliateProfile.findOne({ _id: req.body.id }).then((data) => {
-    if (data) {
+    if (data && req.body.type === "profile") {
       if (data.profile_picture.includes("placeholder") && req.file) {
         data.profile_picture = result.Location;
       }
-      console.log(data.birthdate)
-      console.log(req.body.birthdate)
       if (data.birthdate != req.body.birthdate) {
         data.birthdate = req.body.birthdate;
       }
@@ -42,6 +40,23 @@ console.log(req.body)
       }
       if (data.gender != req.body.gender) {
         data.gender = req.body.gender;
+      }
+      data.save().then(
+        res.status(200),
+        res.json({
+          msg: "Update successful",
+          user_profile: data,
+        })
+      );
+    } else if (data && req.body.type === "contact") {
+      if (data.phone_number != req.body.phone_number) {
+        data.phone_number = req.body.phone_number;
+      }
+      if (data.province != req.body.province) {
+        data.province = req.body.province;
+      }
+      if (data.country != req.body.country) {
+        data.country = req.body.country;
       }
       data.save().then(
         res.status(200),
