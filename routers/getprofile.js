f6ce6403c54f72dbfe276290e08950d6d31b19d5
jonaslemueldatu <jonas.lemuel.datu@eee.upd.edu.ph>
2023-07-16 13:46:30 +0800

    Add brand separation in getprofile API

--- routers/getprofile.js
+++ routers/getprofile.js
@@ -2,34 +2,50 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const affiliateProfile = require("../models/affiliateProfile");
+const brandProfile = require("../models/brandProfile");
 
 router.get("/", async (req, res) => {
-  const data = await affiliateProfile.findOne({ _id: req.query.id });
-  if (data) {
-    data.birthdate
-      ? (date = new Date(data.birthdate).toLocaleDateString("en-CA"))
-      : (date = "");
-    res.status(200),
-      res.json({
-        user_profile: {
-          profile_picture: data.profile_picture,
-          firstname: data.first_name,
-          lastname: data.last_name,
-          birthdate: date,
-          age: data.age,
-          gender: data.gender,
-          email: data.email,
-          phone_number: data.phone_number,
-          country: data.country,
-          province: data.province,
-        },
-        msg: "Profile found!",
-      });
-  } else {
-    res.status(200);
-    res.json({
-      err: "Profile cannot be found!",
-    });
+  console.log(req.query.type);
+  let data = {};
+  switch (req.query.type) {
+    case "Affiliate":
+      data = await affiliateProfile.findOne({ _id: req.query.id });
+      if (data) {
+        data.birthdate
+          ? (date = new Date(data.birthdate).toLocaleDateString("en-CA"))
+          : (date = "");
+        res.status(200),
+          res.json({
+            user_profile: {
+              profile_picture: data.profile_picture,
+              firstname: data.first_name,
+              lastname: data.last_name,
+              birthdate: date,
+              age: data.age,
+              gender: data.gender,
+              email: data.email,
+              phone_number: data.phone_number,
+              country: data.country,
+              province: data.province,
+            },
+            msg: "Profile found!",
+          });
+      } else {
+        res.status(200);
+        res.json({
+          err: "Profile cannot be found!",
+        });
+      }
+    case "Brand":
+      data = await brandProfile.findOne({ _id: req.query.id });
+      if (data) {
+        console.log(data);
+        res.status(200),
+          res.json({
+            msg: "success!",
+          });
+      }
+    default:
   }
 });
 
