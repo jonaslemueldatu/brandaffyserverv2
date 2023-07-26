444e6e20ddb3e9ebb978deeb595c63351c361920
jonaslemueldatu <jonas.lemuel.datu@eee.upd.edu.ph>
2023-07-27 04:49:16 +0800

    Display affiliate list in campaign details

--- routers/dashboard/profilegetlist.js
+++ routers/dashboard/profilegetlist.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const affiliateProfile = require("../../models/affiliateProfile");
 
 router.get("/", async (req, res) => {
+  console.log(req.query)
   const data = await affiliateProfile.find(
     req.query,
     "_id profile_picture first_name last_name email gender age province logged_in province social_tiktok"
   );
+  console.log(data)
   if (data) {
     res.status(200);
     res.json({
