f08c4df8595e37517bb80a7bc60c2b816cb6f758
jonaslemueldatu <jonas.lemuel.datu@eee.upd.edu.ph>
2023-07-27 13:26:27 +0800

    Fixed affiliate display in campaign

---
+++ models/reportsTiktokCampaign.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const reportsTiktokCampaignSchema = new Schema({
+  create_date: {
+    type: Date,
+    default: new Date()
+  },
+  campaign_id: String,
+  video_id: String,
+  affiliate_id: String,
+  like_count: Number,
+  comment_count: Number,
+  share_count: Number,
+  view_count: Number,
+  like_count_diff: Number,
+  comment_count_diff: Number,
+  share_count_diff: Number,
+  view_count_diff: Number,
+});
+
+const reportsTiktokCampaign = mongoose.model(
+  "reportsTiktokCampaignSchema",
+  reportsTiktokCampaignSchema
+);
+
+module.exports = reportsTiktokCampaign;
--- modules/cronupdateactivecampaign.js
+++ modules/cronupdateactivecampaign.js
@@ -1,9 +1,10 @@
 const { CronJob } = require("cron");
 const affiliateCampaignMap = require("../models/affiliateCampaignMap");
 const campaignTiktokVideoMap = require("../models/campaignTiktokVideoMap");
+const reportsTiktokCampaign = require("../models/reportsTiktokCampaign")
 const axios = require("axios");
 
-const updateTiktokVideos = new CronJob("*/30 * * * *", async () => {
+const updateTiktokVideos = new CronJob("* */30 * * * *", async () => {
   console.log("Updating active Video listing, timestamp: ", new Date());
 
   const data = await affiliateCampaignMap.aggregate([
@@ -55,7 +56,6 @@ const updateTiktokVideos = new CronJob("*/30 * * * *", async () => {
       },
     },
   ]);
-  let counter = 0;
   if (data.length > 0) {
     const vidqueryEndpoint =
       "https://open.tiktokapis.com/v2/video/query/?fields=id,like_count,comment_count,share_count,view_count";
@@ -82,12 +82,26 @@ const updateTiktokVideos = new CronJob("*/30 * * * *", async () => {
               campaign_id: affiliateCampaign.campaign_id,
               video_id: videos.id,
             });
+            const newReportsTiktokCampaign = new reportsTiktokCampaign({
+              campaign_id: affiliateCampaign.campaign_id,
+              video_id: videos.id,
+              like_count: videos.like_count,
+              comment_count: videos.comment_count,
+              share_count: videos.share_count,
+              view_count: videos.view_count,
+              like_count_diff: videos.like_count - campaignTiktok.like_count,
+              comment_count_diff:
+                videos.comment_count - campaignTiktok.comment_count,
+              share_count_diff: videos.share_count - campaignTiktok.share_count,
+              view_count_diff: videos.view_count - campaignTiktok.view_count,
+            });
+            await newReportsTiktokCampaign.save();
+
             (campaignTiktok.like_count = videos.like_count),
               (campaignTiktok.comment_count = videos.comment_count),
               (campaignTiktok.share_count = videos.share_count),
               (campaignTiktok.view_count = videos.view_count),
               await campaignTiktok.save();
-            counter++;
           });
         } else {
           console.log("Failed to get data");
