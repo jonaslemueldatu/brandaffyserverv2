e5ddb02bd7741d6a5166f2ae12261b96e32a4bbe
jonaslemueldatu <jonas.lemuel.datu@eee.upd.edu.ph>
2023-07-30 16:40:05 +0800

    Test

--- routers/payment/subscriptioncreatepaymentmethod.js
+++ routers/payment/subscriptioncreatepaymentmethod.js
@@ -4,26 +4,27 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
-    console.log(req.query.link);
-    let apiKey =
-      "xnd_development_NZvxlmIGcBTUDPt65l4IJlj6ZosrwEISS3chz4eS2KXXNojRnIiRTxGMyq47ll:";
-    const reusability = await axios.get(
-      req.query.link,
-      {},
-      {
-        headers: {
-          Authorization: `Basic ${btoa(apiKey + ":")}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    console.log(reusability.data);
-    res.status(200);
-    res.json({
-      data: reusability.data,
-    });
+    console.log(req.body);
+    // let apiKey =
+    //   "xnd_development_NZvxlmIGcBTUDPt65l4IJlj6ZosrwEISS3chz4eS2KXXNojRnIiRTxGMyq47ll:";
+    // const reusability = await axios.get(
+    //   req.query.link,
+    //   {},
+    //   {
+    //     headers: {
+    //       Authorization: `Basic ${btoa(apiKey + ":")}`,
+    //       "Content-Type": "application/json",
+    //     },
+    //   }
+    // );
+    // console.log(reusability.data);
+    // res.status(200);
+    // res.json({
+    //   data: reusability.data,
+    // });
+    res.send("Hello World")
   } catch (error) {
     console.log(`router, ${error.message}`);
   }
