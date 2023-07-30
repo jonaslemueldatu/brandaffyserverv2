a6ccbdc3a37624b0a3684e7b43fdacaf6204f1ae
jonaslemueldatu <jonas.lemuel.datu@eee.upd.edu.ph>
2023-07-30 21:47:26 +0800

    Hello world

--- webhooks/xenditpaymentmethodcallback.js
+++ webhooks/xenditpaymentmethodcallback.js
@@ -8,18 +8,19 @@ const brandSubscription = require("../models/brandSubscription");
 router.post("/", async (req, res) => {
   try {
     res.status(200);
+    res.json({
+      msg: "Successfully received callback"
+    })
     console.log(req.body.data.reference_id);
     const ObjectId = new mongoose.Types.ObjectId(req.body.data.reference_id);
     const subscriptionData = await brandSubscription.findOne({
       _id: ObjectId,
     });
-    console.log(subscriptionData);
-
     subscriptionData.plan_payment_methods.push(
       req.body.data.ewallet.channel_code
     );
     subscriptionData.plan_payment_methods_object.push(req.body);
-    console.log(subscriptionData);
+    await subscriptionData.save();
   } catch (error) {
     console.log(`xenditpaymentmethodcallback.js, ${error.message}`);
   }
