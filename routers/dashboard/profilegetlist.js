97c7aa79dad1b6777a0a73d35d377186a636d448
jonaslemueldatu <jonas.lemuel.datu@eee.upd.edu.ph>
2023-07-31 09:35:11 +0800

    Sort functionality

--- routers/dashboard/profilegetlist.js
+++ routers/dashboard/profilegetlist.js
@@ -3,15 +3,107 @@ const router = express.Router();
 const creatorProfile = require("../../models/creatorProfile");
 
 router.get("/", async (req, res) => {
-  const data = await creatorProfile.find(
-    req.query,
-    "_id profile_picture first_name last_name occupation niche email gender age province logged_in province social_tiktok social_tiktok_follower_count"
-  );
+  //Apply search on list of Creators
+  const search = req.query.search || "";
+  const data = await creatorProfile.aggregate([
+    {
+      $match: {
+        $or: [
+          {
+            first_name: {
+              $regex: search,
+              $options: "i",
+            },
+          },
+          {
+            last_name: {
+              $regex: search,
+              $options: "i",
+            },
+          },
+          {
+            email: {
+              $regex: search,
+              $options: "i",
+            },
+          },
+          {
+            occupation: {
+              $regex: search,
+              $options: "i",
+            },
+          },
+          {
+            niche: {
+              $regex: search,
+              $options: "i",
+            },
+          },
+        ],
+      },
+    },
+    {
+      $sort: {
+        social_tiktok_follower_count: -1,
+      },
+    },
+    {
+      $skip: parseInt(req.query.page) * parseInt(req.query.limit),
+    },
+    {
+      $limit: parseInt(req.query.limit),
+    },
+  ]);
+
+  //Get total number for pagination
+  const totalProfile = await creatorProfile.aggregate([
+    {
+      $match: {
+        $or: [
+          {
+            first_name: {
+              $regex: search,
+              $options: "i",
+            },
+          },
+          {
+            last_name: {
+              $regex: search,
+              $options: "i",
+            },
+          },
+          {
+            email: {
+              $regex: search,
+              $options: "i",
+            },
+          },
+          {
+            occupation: {
+              $regex: search,
+              $options: "i",
+            },
+          },
+          {
+            niche: {
+              $regex: search,
+              $options: "i",
+            },
+          },
+        ],
+      },
+    },
+    {
+      $count: "Total",
+    },
+  ]);
+
   if (data) {
     res.status(200);
     res.json({
       msg: "Successfully pulled list of creators",
       creator_list: data,
+      total: totalProfile.length > 0 ? totalProfile[0].Total : 0,
     });
   }
 });
